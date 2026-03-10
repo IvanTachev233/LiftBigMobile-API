4b7f09e7c4280f662ca01f2b2a0d4da7dafcdab0
Ivan <ivant@oleo.co.uk>
2026-03-10 08:03:39 +0000

    cors update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.getHttpAdapter().getInstance().set('etag', false);
+  app.enableCors({
+    origin: [
+      'capacitor://localhost',
+      'http://localhost:4200',
+      'http://localhost:8100',
+      'https://liftbigmobile-api-a4eqcsf6f8chb3dp.westeurope-01.azurewebsites.net',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
